bf97fb7dc2951c7f0f5c24dc7b7a47e3d5bafa95
skoenig3 <sethkoenig@gmail.com>
2017-01-12 17:56:03 -0800

    bug found in shifting nan portion of code...bug is now fixed so rotates non-NaN values while maintaining position of NaN values in matrix

--- circshift_row.m
+++ circshift_row.m
@@ -3,18 +3,25 @@ function B = circshift_row(A)
 % preserves matrix otherwise. Function is faster than Matlabs's circhift
 % if done row by row and deals with NaNs and shifts appropriately.
 % assumes NaNs are at the end of the data not sporadically  distributed!!!!
+%
+% Code rechecked for bugs January 11, 2017 SDK
+
 if isempty(A)
     B = A;
-elseif any(any(isnan(A))) %if their are NaNs must be fillers to speed up computation
+elseif any(any(isnan(A))) 
+    %ignore NaNs when shifting spike trains and preserve indeces, 
+    %usually NaNs at start or end of indeces
     [m, n] = size(A);
     B = NaN(m, n);
     
     for i = (1 : m)
         n = sum(~isnan(A(i,:)));
-        if n~=0
-            temp = A(i,1:n);
-            D = randi(n);
-            B(i,1:n) = [temp(n - D + 1 : n) temp(1 : n - D)];
+        if n~=0 %if not all NaNs
+            nanind = isnan(A(i,:)); %find which indeces are NaNs
+            temp = A(i,~nanind); %take non-NaN values
+            D = randi(length(temp)); %shift only non-NaN values
+            temp = [temp(n - D + 1 : n) temp(1 : n - D)]; %shift only non-NaN values
+            B(i,~nanind) = temp; %presereve NaN ind
         end
     end
 else
