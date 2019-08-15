a146e3fd091eb817b991d7f1f98d6c7943412e4d
skoenig3 <sethkoenig@gmail.com>
2019-08-15 08:36:22 -0500

    no message

---
+++ ChiSquareTest_of_Proportions.m
@@ -0,0 +1,32 @@
+
+% Observed data
+n1 = 29; N1 = 81;
+n2 = 23; N2 = 116;
+x1 = [repmat('a',N1,1); repmat('b',N2,1)];
+x2 = [repmat(1,n1,1); repmat(2,N1-n1,1); repmat(1,n2,1); repmat(2,N2-n2,1)];
+[tbl,chi2stat,pval] = crosstab(x1,x2)
+
+%%
+n1 =34; N1 = 347;
+n2 = 17; N2 = 347;
+x1 = [repmat('a',N1,1); repmat('b',N2,1)];
+x2 = [repmat(1,n1,1); repmat(2,N1-n1,1); repmat(1,n2,1); repmat(2,N2-n2,1)];
+[tbl,chi2stat,pval] = crosstab(x1,x2)
+%%
+n1 = 18; N1 = 180;
+n2 = 9; N2 = 180;
+x1 = [repmat('a',N1,1); repmat('b',N2,1)];
+x2 = [repmat(1,n1,1); repmat(2,N1-n1,1); repmat(1,n2,1); repmat(2,N2-n2,1)];
+[tbl,chi2stat,pval] = crosstab(x1,x2)
+%%
+n1 = 9; N1 = 78;
+n2 = 4; N2 = 78;
+x1 = [repmat('a',N1,1); repmat('b',N2,1)];
+x2 = [repmat(1,n1,1); repmat(2,N1-n1,1); repmat(1,n2,1); repmat(2,N2-n2,1)];
+[tbl,chi2stat,pval] = crosstab(x1,x2)
+%%
+n1 = 18; N1 = 180;
+n2 = 9; N2 = 180;
+x1 = [repmat('a',N1,1); repmat('b',N2,1)];
+x2 = [repmat(1,n1,1); repmat(2,N1-n1,1); repmat(1,n2,1); repmat(2,N2-n2,1)];
+[tbl,chi2stat,pval] = crosstab(x1,x2)
